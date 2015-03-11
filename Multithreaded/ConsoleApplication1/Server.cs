7e6992ad46555b39a8bb99e5219d8627ff8c1a35
Sarah Busert <sarah.busert@hs-furtwangen.de>
2015-03-11 20:05:57 +0100

    Added Multithreaded/-client Client/Server-App

---
+++ Multithreaded/ConsoleApplication1/Server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server
+{
+    public class server
+    {
+        ManualResetEvent tcpClientConnected = new ManualResetEvent(false);
+
+        void ProcessIncomingData(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            StringBuilder sb = new StringBuilder();
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                int i;
+                while ((i = stream.ReadByte()) != 0)
+                {
+                    sb.Append((char)i);
+                }
+
+                string reply = "ack: " + sb.ToString() + '\0';
+                stream.Write(Encoding.ASCII.GetBytes(reply), 0, reply.Length);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        void ProcessIncomingConnection(IAsyncResult ar)
+        {
+            TcpListener listener = (TcpListener)ar.AsyncState;
+            TcpClient client = listener.EndAcceptTcpClient(ar);
+
+            //Brings Thread to Backgrounhd Thread - listen to other Clients
+            ThreadPool.QueueUserWorkItem(ProcessIncomingData, client);
+            tcpClientConnected.Set();
+        }
+
+
+        public static string IpList()
+        {
+            IPHostEntry Host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in Host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return string.Empty;
+        }
+        
+        
+        public void start()
+        {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(IpList()), 5000);
+            TcpListener listener = new TcpListener(endpoint);
+            listener.Start();
+
+            Console.WriteLine("The local End point is  :" +
+                              listener.LocalEndpoint);
+
+            while (true)
+            {
+                tcpClientConnected.Reset();
+                listener.BeginAcceptTcpClient(new AsyncCallback(ProcessIncomingConnection), listener);
+                tcpClientConnected.WaitOne();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            server s = new server();
+            s.start();
+        }
+    }
+}
---
+++ Multithreaded/ConsoleApplication2/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    class Program
+    {
+        
+        const int NumberOfThreads = 10;
+        void Work(object obj)
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("192.168.0.14"), 5000);
+            TcpClient client = new TcpClient();
+            client.Connect(ep);
+
+            StringBuilder sb = new StringBuilder();
+            using (NetworkStream stream = client.GetStream())
+            {
+                string request = "Test" + '\0';
+                Console.WriteLine("sent: " + request);
+                stream.Write(Encoding.ASCII.GetBytes(request), 0, request.Length);
+
+                int i;
+                while ((i = stream.ReadByte()) != 0)
+                {
+                    sb.Append((char)i);
+                }
+            }
+            client.Close();
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        void start()
+        {
+            for (int i = 0; i < NumberOfThreads; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Work);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.start();
+
+            //press any key to exit
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
