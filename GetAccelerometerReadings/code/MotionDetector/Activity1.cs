5e5656847fbb13b351309e1ded2b4f4d2bc88ff6
bJohnny <der.jbrown@gmail.com>
2015-03-19 21:06:00 +0100

    Fixed RegisterListener & made sense out of sensor data

--- GetAccelerometerReadings/code/MotionDetector/Activity1.cs
+++ GetAccelerometerReadings/code/MotionDetector/Activity1.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading;
 using Android.App;
 using Android.Content.PM;
@@ -44,7 +46,11 @@ namespace MotionDetector
         {
             base.OnResume();
             _sensorManager.RegisterListener(this, _sensorManager.GetDefaultSensor(SensorType.Accelerometer),
-                SensorDelay.Ui);
+               SensorDelay.Ui);
+
+            //Register a the Gyroscope
+
+            //_sensorManager.RegisterListener(this, _sensorManager.GetDefaultSensor(SensorType.Gyroscope), SensorDelay.Ui);
         }
 
         protected override void OnPause()
@@ -60,21 +66,71 @@ namespace MotionDetector
 
         public void OnSensorChanged(SensorEvent e)
         {
-            lock (SyncLock)
+
+
+            if (e.Sensor.Type == SensorType.Accelerometer)
             {
-                var text = new StringBuilder("x = ")
-                    .Append(e.Values[0])
-                    .Append(", y=")
-                    .Append(e.Values[1])
-                    .Append(", z=")
-                    .Append(e.Values[2]);
-                _sensorTextView.Text = text.ToString();
-                MessageString = text.ToString();
+                var calibrationValue = SensorManager.StandardGravity;
+                lock (SyncLock)
+                {
+                    var text = new StringBuilder("Sensor: Accelerometer. \n Beschleunigung entlang der Achsen")
+                        .Append("\nx : ")
+                        .Append(Math.Round(e.Values[0], 2))
+                        .Append(" Lateral")
+                        .Append("\ny : ")
+                        .Append(Math.Round(e.Values[1], 2))
+                        .Append(" Longitudinal")
+                        .Append("\nz : ")
+                        .Append(Math.Round(e.Values[2], 2))
+                        .Append(" Vertikal (mit Erdanziehung)")
+                        .Append("\nAccuracy : ")
+                        .Append(e.Accuracy);
+                    _sensorTextView.Text = text.ToString();
+                    MessageString = text.ToString();
+
+                    // Calculate total acceleration w positive values and eliminate gravity
+                    var SumOfSq = Math.Pow(e.Values[0], 2) + Math.Pow(e.Values[1], 2) + Math.Pow(e.Values[2], 2);
+                    var mach = Math.Pow(SumOfSq, .5) - calibrationValue;
+                    RunOnUiThread(() => 
+                        _testView.Text = "Beschleunigung gesamt: " + mach.ToString());
+                } 
             }
 
-            var test = new StringBuilder("so schreibe ich etwas auf den Screen");
-            _testView.Text = test.ToString();
+            //if (e.Sensor.Type == SensorType.Gyroscope)
+            //{
+            //    lock (SyncLock)
+            //    {
+            //        var text = new StringBuilder("Sensor: Gyorscope. \n Orientierung in Grad entlang der Achsen")
+            //                        .Append("\nx : ")
+            //                        .Append(e.Values[0])
+            //                        .Append("\ny : ")
+            //                        .Append(e.Values[1])
+            //                        .Append("\nz : ")
+            //                        .Append(e.Values[2])
+            //                        .Append("\nAccuracy : ")
+            //                        .Append(e.Accuracy);
+            //        _sensorTextView.Text = text.ToString();
+            //        MessageString = text.ToString(); 
+            //    }
+            //}
+
+            
+
+            //var test = new StringBuilder("so schreibe ich etwas auf den Screen");
+            //_testView.Text = test.ToString();
          }
+
+        public void OnAccuracyChanged(Sensor sensor, int accuracy)
+        {
+            if (sensor.Type == Android.Hardware.SensorType.Accelerometer)
+            {
+                if (Android.Hardware.SensorStatus.AccuracyHigh ==
+         (Android.Hardware.SensorStatus)accuracy)
+                {
+
+                }
+            }
+        }
 
         //public override void OnBackPressed()
         //{
